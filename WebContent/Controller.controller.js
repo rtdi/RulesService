333b23378dcfe02258e419997969d10812d06ea9
werner daehn <werner.daehn@googlemail.com>
2025-02-16 19:07:35 +0100

    Separate status page

--- WebContent/Controller.controller.js
+++ WebContent/Controller.controller.js
@@ -4,9 +4,6 @@ sap.ui.define([ "sap/ui/core/mvc/Controller"], function(Controller) {
 	return Controller.extend("io.rtdi.bigdata.rulesservice.Controller", {
 
 		onInit : function() {
-			var oModel = new sap.ui.model.json.JSONModel();
-			oModel.loadData("./rest/config/service");
-			this.getView().setModel(oModel);
 		}
 
 	});
